57d40bbfe56d9c1db7dc94dba2091c50158295fd
yoshiki <yoshiki0911@gmail.com>
2020-11-07 01:08:24 +0900

    #1 add Setting-classes

--- azutils/databricks/_databricks.py
+++ azutils/databricks/_databricks.py
@@ -100,3 +100,61 @@ class DataBricksRunningTime:
             f"  * cluster_num: {self.current_num_workers}"
         ]
         return "\n".join(s_list)
+
+
+class DatabricksSetting:
+    def __init__(self, start_timestamp, end_timestamp, databricks: Databricks):
+        self.start_timestamp = start_timestamp
+        self.end_timestamp = end_timestamp
+        self.databricks = databricks
+        self.start_dt = datetime.fromtimestamp(int(self.start_timestamp / 1000), timezone(timedelta(hours=9)))
+        self.start_ymd = self.start_dt.strftime("%Y-%m-%d")
+        self.start_hms = self.start_dt.strftime("%H:%M:%S")
+        self.end_dt = datetime.fromtimestamp(int(self.end_timestamp / 1000), timezone(timedelta(hours=9)))
+        self.end_ymd = self.end_dt.strftime("%Y-%m-%d")
+        self.end_hms = self.end_dt.strftime("%H:%M:%S")
+
+    @staticmethod
+    def get_from_databricks_event(databricks_event: List[DatabricksEvents]):
+        pass
+
+    def __str__(self):
+        s_list = [
+            f"* {self.start_ymd}T{self.start_hms} {self.end_ymd}T{self.end_hms}",
+            f"  * driver_node_type: {self.databricks.driver_node_type_id}",
+            f"  * node_type: {self.databricks.node_type_id}",
+        ]
+        return "\n".join(s_list)
+
+
+class DatabricksSettingHistory:
+    def __init__(self):
+        self._databricks_setting_list: List[DatabricksSetting] = []
+
+    def append(self, databricks_setting: DatabricksSetting):
+        self._databricks_setting_list.append(databricks_setting)
+
+    def get_at(self, timestamp) -> Databricks:
+        initial_setting = self._databricks_setting_list[0]
+        initial_cluster = initial_setting.databricks
+        initial_timestamp = initial_setting.start_timestamp
+        latest_cluster = initial_setting.databricks
+        latest_timestamp = initial_setting.end_timestamp
+
+        # get latest or initial cluster
+        for cluster in self._databricks_setting_list[1:]:
+            if initial_timestamp >= cluster.end_timestamp:
+                initial_timestamp = cluster.start_timestamp
+                initial_cluster = cluster
+            elif latest_timestamp <= cluster.start_timestamp:
+                latest_timestamp = cluster.end_timestamp
+                latest_cluster = cluster
+            elif cluster.start_timestamp <= timestamp <= cluster.end_timestamp:
+                return cluster.databricks
+
+        if timestamp <= initial_timestamp:
+            return initial_cluster
+        elif timestamp >= latest_timestamp:
+            return latest_cluster
+        else:
+            raise ValueError("Out of range.")
