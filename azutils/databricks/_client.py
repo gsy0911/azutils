097f390752eba5f701bde63d6ee433d7260cb69a
yoshiki <yoshiki0911@gmail.com>
2020-11-06 22:56:33 +0900

    #1 modify clusters_events()

--- azutils/databricks/_client.py
+++ azutils/databricks/_client.py
@@ -1,5 +1,5 @@
 import requests
-from typing import Optional
+from typing import List, Optional
 
 
 class DatabricksClient:
@@ -19,12 +19,32 @@ class DatabricksClient:
         res = requests.get(url, headers=self._headers)
         return res.json()
 
-    def clusters_events(self, cluster_id: str, timestamp: Optional[int] = None, offset: Optional[int] = None) -> dict:
+    def clusters_events(
+            self,
+            cluster_id: str,
+            page_limit: int = 500) -> List[dict]:
+        result_list = []
+        response = self._clusters_events(cluster_id=cluster_id)
+        result_list.extend(response['events'])
+        for _ in range(page_limit):
+            next_page = response['next_page']
+            print(next_page)
+            end_time = next_page['end_time']
+            offset = next_page['offset']
+            response = self._clusters_events(cluster_id=cluster_id, end_time=end_time, offset=offset)
+            result_list.extend(response['events'])
+        return result_list
+
+    def _clusters_events(
+            self,
+            cluster_id: str,
+            end_time: Optional[int] = None,
+            offset: Optional[int] = None) -> dict:
         url = f"{self._base_url}/clusters/events"
         payload = {"cluster_id": cluster_id}
-        if timestamp is not None:
-            payload['timestamp'] = timestamp
+        if end_time is not None:
+            payload['end_time'] = end_time
         if offset is not None:
             payload['offset'] = offset
-        res = requests.post(url, json=payload, headers=self._headers)
-        return res.json()
+        response = requests.post(url, json=payload, headers=self._headers)
+        return response.json()
