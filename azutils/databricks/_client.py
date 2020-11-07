3e289138f19cc6870b53d3f58b5c1c98f69b6808
yoshiki <yoshiki0911@gmail.com>
2020-11-07 11:41:31 +0900

    #1 modify cluster_event function

--- azutils/databricks/_client.py
+++ azutils/databricks/_client.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 from typing import List, Optional, Union
 
@@ -32,23 +33,55 @@ class DatabricksClient:
         else:
             return [Databricks(cluster) for cluster in response.json()['clusters']]
 
-    @cached(cache={})
+    @staticmethod
+    def _convert_datetime_to_milli_epoch(input_time):
+        """
+        Convert str or epoch-int to epoch-milli-int
+
+        Args:
+            input_time: ISO8601-format datetime, or epochtime
+
+        Returns:
+
+        """
+        if type(input_time) is str:
+            start_timestamp = int(datetime.fromisoformat(input_time).timestamp() * 1000)
+            return start_timestamp
+        elif type(input_time) is int:
+            if len(str(input_time)) == 13:
+                return input_time
+            elif len(str(input_time)) == 10:
+                return input_time * 1000
+
     def clusters_events(
             self,
             cluster_id: str,
+            start_time: Optional[Union[int, str]] = None,
+            end_time: Optional[Union[int, str]] = None,
             page_limit: int = 500,
             raw=False) -> Union[List[dict], List[DatabricksEvents]]:
+
+        start_timestamp = self._convert_datetime_to_milli_epoch(start_time)
+        end_timestamp = self._convert_datetime_to_milli_epoch(end_time)
         result_list = []
-        response = self._clusters_events(cluster_id=cluster_id)
+        response = self._clusters_events(
+            cluster_id=cluster_id,
+            start_time=start_timestamp,
+            end_time=end_timestamp
+        )
         result_list.extend(response['events'])
         for _ in range(page_limit):
             if "next_page" not in response:
                 break
             next_page = response['next_page']
             print(next_page)
-            end_time = next_page['end_time']
+            end_timestamp = next_page['end_time']
             offset = next_page['offset']
-            response = self._clusters_events(cluster_id=cluster_id, end_time=end_time, offset=offset)
+            response = self._clusters_events(
+                cluster_id=cluster_id,
+                start_time=start_timestamp,
+                end_time=end_timestamp,
+                offset=offset)
             result_list.extend(response['events'])
         if raw:
             return result_list
